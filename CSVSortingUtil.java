2339fb895a3a1e87c8716eafac7dbf177236d7f0
Ункуров Роман <runkurov@ntdev.ru>
2022-07-13 20:54:49 +0300

    refactor

--- CSVSortingUtil.java
+++ CSVSortingUtil.java
@@ -14,12 +14,8 @@ public class CSVSortingUtil {
 
 
     public static void main(String[] args) throws IOException {
-        readAllLinesFromFile(CSV);
-        for(String aStudentString: listStrings){
-            System.out.println(aStudentString +"\n");
-        }
 
-        convertToStudents(listStrings);
+        ArrayList<CSVRepresentation> csvRepresentations = convertAndSortCSV(readAllLinesFromFile(CSV));
 
         writeAllLinesToFile(CSV_NEW, listStrings);
 
@@ -39,32 +35,32 @@ public class CSVSortingUtil {
 
     }
 
-    static ArrayList<CSVRepresentation> convertToStudents(List<String> studentsStrings) {
+    static ArrayList<CSVRepresentation> convertAndSortCSV(List<String> csvStrings) {
         ArrayList<CSVRepresentation> csvRepresentations = new ArrayList<>();
-        studentsStrings.remove(0);
-        for(String studentString : studentsStrings) {
-            String[] parts = studentString.split(";");
-            int FID = Integer.parseInt(parts[0]);
-            String SERIAL_NUM = parts[1];
-            String MEMBER_CODE = parts[2];
-            String ACCT_TYPE = parts[3];
-            String OPENED_DT = parts[4];
-            String ACCT_RTE_CDE = parts[5];
-            String REPORTING_DT = parts[6];
-            String CREDIT_LIMIT = parts[7];
+        csvStrings.remove(0);
+        for(String csvString : csvStrings) {
+            String[] parts = csvString.split(";");
+            int fid = Integer.parseInt(parts[0]);
+            String serialNum = parts[1];
+            String memberCode = parts[2];
+            String accType = parts[3];
+            String openedDt = parts[4];
+            String accRteCde = parts[5];
+            String reportingDt = parts[6];
+            String creditLimit = parts[7];
             CSVRepresentation representationNode = new CSVRepresentation();
-            representationNode.setFID(String.valueOf(FID));
-            representationNode.setSERIAL_NUM(SERIAL_NUM);
-            representationNode.setMEMBER_CODE(MEMBER_CODE);
-            representationNode.setACCT_TYPE(ACCT_TYPE);
-            representationNode.setOPENED_DT(OPENED_DT);
-            representationNode.setACCT_RTE_CDE(ACCT_RTE_CDE);
-            representationNode.setREPORTING_DT(REPORTING_DT);
-            representationNode.setCREDIT_LIMIT(CREDIT_LIMIT);
+            representationNode.setFid(fid);
+            representationNode.setSerialNum(serialNum);
+            representationNode.setMemberCode(memberCode);
+            representationNode.setAccType(accType);
+            representationNode.setOpenedDt(openedDt);
+            representationNode.setAccRteCde(accRteCde);
+            representationNode.setReportingDt(reportingDt);
+            representationNode.setCreditLimit(creditLimit);
             csvRepresentations.add(representationNode);
         }
 
-        csvRepresentations.sort(Comparator.comparing(CSVRepresentation::getFID));
+        csvRepresentations.sort(Comparator.comparing(CSVRepresentation::getFid));
         return csvRepresentations;
     }
 
@@ -79,15 +75,19 @@ public class CSVSortingUtil {
 
 
     @Data
-    private static class CSVRepresentation {
-        private String FID;
-        private String SERIAL_NUM;
-        private String MEMBER_CODE;
-        private String ACCT_TYPE;
-        private String OPENED_DT;
-        private String ACCT_RTE_CDE;
-        private String REPORTING_DT;
-        private String CREDIT_LIMIT;
+    private static class CSVRepresentation implements Comparable<CSVRepresentation> {
+        private int fid;
+        private String serialNum;
+        private String memberCode;
+        private String accType;
+        private String openedDt;
+        private String accRteCde;
+        private String reportingDt;
+        private String creditLimit;
 
+        @Override
+        public int compareTo(CSVRepresentation o) {
+            return this.fid - o.getFid();
+        }
     }
 }
